e1875c547b86e48f0c28078afe90a3a9efa4de1c
Paul Parker <paulparker1994@gmail.com>
2024-12-15 03:25:36 +0000

    Prepare for deployment

--- app/api/delete/route.ts
+++ app/api/delete/route.ts
@@ -16,9 +16,8 @@ async function resetEbookSequence() {
   // Set the next sequence ID for 'Ebook' table, defaulting to 1 if no records exist
   const nextId = (maxId._max.id || 0) + 1; 
   // Set 'Ebook' sequence to the next ID
-  await prisma.$executeRaw`
-  SELECT setval('public."Ebook_id_seq"', ${nextId}, false);
-`;
+  await prisma.$executeRawUnsafe(`
+    SELECT setval('public."Ebook_id_seq"', ${nextId}, false); 
   `);
 }
 
